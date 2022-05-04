abeddbeee53178fc211a0d91f45e3284b905d204
song <shinobu2624@unist.ac.kr>
2022-05-04 13:36:50 +0900

    init

---
+++ src/main/java/com/song/simplebb/SimpleBbApplication.java
@@ -0,0 +1,13 @@
+package com.song.simplebb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/song/simplebb/SimpleBbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song.simplebb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
